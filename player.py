ff8f5e6ead6ab75394b82ad52e9df5ee459e6807
Eszter Kobor <kobor.eszter@gmail.com>
2019-12-16 10:39:51 +0100

    Add bet to call

--- player.py
+++ player.py
@@ -1,12 +1,21 @@
 import json
 
+
 class Player:
     VERSION = "1.0"
 
-    def betRequest(self, game_state):
-        data = json.loads(game_state)
 
-        return 200
+    def betRequest(self, game_state):
+        game_data = json.loads(game_state)
+
+        in_action = game_state["in_action"] # our player's index
+
+        current_buy_in = game_state["current_buy_in"]
+
+        bet_already_made_me = game_data["players"][in_action]["bet"] # players[in_action][bet]
+        to_call = current_buy_in - bet_already_made_me
+        return to_call
+
 
     def showdown(self, game_state):
         pass
