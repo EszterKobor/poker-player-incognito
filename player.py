8258eacf680467a9b7d32dc3d1b8510da96129fc
Eszter Kobor <kobor.eszter@gmail.com>
2019-12-16 10:28:45 +0100

    Add fix bet

--- player.py
+++ player.py
@@ -1,9 +1,12 @@
+import json
 
 class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
-        return 0
+        data = json.loads(game_state)
+
+        return 200
 
     def showdown(self, game_state):
         pass
