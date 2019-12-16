33d4a1a0f66a018588edeafb9a46a71874845c9a
Eszter Kobor <kobor.eszter@gmail.com>
2019-12-16 10:50:27 +0100

    Add bet to raise

--- player.py
+++ player.py
@@ -8,13 +8,15 @@ class Player:
     def betRequest(self, game_state):
         game_data = json.loads(game_state)
 
-        in_action = game_state["in_action"] # our player's index
-
+        in_action = game_state["in_action"]  # our player's index
         current_buy_in = game_state["current_buy_in"]
+        minimum_raise = game_state["minimum_raise"]
+        bet_already_made_me = game_data["players"][in_action]["bet"]  # players[in_action][bet]
 
-        bet_already_made_me = game_data["players"][in_action]["bet"] # players[in_action][bet]
         to_call = current_buy_in - bet_already_made_me
-        return to_call
+        to_raise = to_call + minimum_raise
+
+        return to_raise
 
 
     def showdown(self, game_state):
