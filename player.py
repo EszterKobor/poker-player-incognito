b29a4689e42ab7fd7140f4a67a8d2c23a685ad6a
Paebo <petibors2@gmail.com>
2019-12-16 11:31:08 +0100

    Quick return bet

--- player.py
+++ player.py
@@ -6,19 +6,19 @@ class Player:
 
 
     def betRequest(self, game_state):
-        game_data = json.loads(game_state)
+        #game_data = json.loads(game_state)
 
-        in_action = game_data["in_action"]  # our player's index
-        current_buy_in = game_data["current_buy_in"]
-        minimum_raise = game_data["minimum_raise"]
-        me = game_data["players"][in_action]
-        bet_already_made_me = me["bet"]  # players[in_action][bet]
-        my_cards = me["hole_cards"]
-        community_cars = game_data["community_cards"]
+        #in_action = game_data["in_action"]  # our player's index
+        #current_buy_in = game_data["current_buy_in"]
+        #minimum_raise = game_data["minimum_raise"]
+        #me = game_data["players"][in_action]
+        #bet_already_made_me = me["bet"]  # players[in_action][bet]
+        #my_cards = me["hole_cards"]
+        #community_cars = game_data["community_cards"]
         # card_a_can_choose_from =
 
-        to_call = current_buy_in - bet_already_made_me
-        to_raise = to_call + minimum_raise
+        #to_call = current_buy_in - bet_already_made_me
+        #to_raise = to_call + minimum_raise
 
 
 
