890e58548025719e50ad17ba029b455489743c22
Paebo <petibors2@gmail.com>
2019-12-16 14:23:05 +0100

    Number_of_pairs done

--- player.py
+++ player.py
@@ -4,6 +4,29 @@ import json
 class Player:
     VERSION = "1.0"
 
+    def get_number_of_pairs(self, cards_to_choose_from):
+        ranks = []
+        num_of_pairs = 0
+        for card in range(len(cards_to_choose_from)):
+            for cardkey in cards_to_choose_from[card].keys():
+                if cardkey == "rank":
+                    ranks.append(cards_to_choose_from[card][cardkey])
+        print ranks
+
+
+        for rank in ranks:
+            if ranks.count(rank) > 1:
+                ranks.remove(rank)
+                ranks.remove(rank)
+
+                num_of_pairs += 1
+
+        for rank in ranks:
+            if ranks.count(rank) > 1:
+                num_of_pairs += 1
+
+        print num_of_pairs
+        return num_of_pairs
 
     def betRequest(self, game_state):
         game_data = game_state
@@ -28,10 +51,19 @@ class Player:
             if player["status"] != "out":
                 actives_couter += 1
 
+        if self.get_number_of_pairs(cards_to_choose_from) > 1:
+            to_raise += 100
+            return to_raise
+
         if actives_couter > 2:
             return 0
         else:
             return to_raise
 
+
     def showdown(self, game_state):
         pass
+
+
+
+
